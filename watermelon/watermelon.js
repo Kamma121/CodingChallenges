0fa7399fdc59e76c63ea44015938212248f847ee
Kamil <KamilSodo132@gmail.com>
2023-03-31 13:02:29 +0200

    watermelon task solution

--- watermelon/watermelon.js
+++ watermelon/watermelon.js
@@ -1,1 +1,13 @@
-let a = 0;
+function calcWatermelon(watermelonWeight) {
+    let wrong = "NO";
+    let correct = "YES";
+    if (watermelonWeight < 1 || watermelonWeight > 100) {
+        return wrong;
+    }
+    if ((watermelonWeight / 2) % 2 === 0 || watermelonWeight % 2 === 0) {
+        return correct;
+    }
+    return wrong;
+}
+
+calcWatermelon(150);
