99f0f594ad2a48dec71933b4f5aed3acfc2b5553
Kamil <KamilSodo132@gmail.com>
2023-04-15 14:06:42 +0200

    solution for running-sum

---
+++ running-sum/running-sum.js
@@ -0,0 +1,9 @@
+//https://leetcode.com/problems/running-sum-of-1d-array/description/
+
+function calcRunningSum(nums) {
+    let runningSum = nums.slice();
+    for (let i = 1; i < nums.length; i++) {
+        runningSum[i] = nums[i] + runningSum[i - 1];
+    }
+    return runningSum;
+};
